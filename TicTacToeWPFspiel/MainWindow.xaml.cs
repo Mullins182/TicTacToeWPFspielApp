81d36d064ca0d9a4b36586fc63034c49180c45a0
Mullins182 <Mullins182@t-online.de>
2023-11-29 10:58:55 +0100

    debugging ...

--- TicTacToeWPFspiel/MainWindow.xaml.cs
+++ TicTacToeWPFspiel/MainWindow.xaml.cs
@@ -20,5 +20,59 @@ namespace TicTacToeWPFspiel
         {
             InitializeComponent();
         }
+
+        private void BA1_Click(object sender, RoutedEventArgs e)
+        {
+            BA1.Visibility  = Visibility.Hidden;
+            BXA1.Visibility = Visibility.Visible;
+        }
+
+        private void BA2_Click(object sender, RoutedEventArgs e)
+        {
+            BA2.Visibility  = Visibility.Hidden;
+            BXA2.Visibility = Visibility.Visible;
+        }
+
+        private void BA3_Click(object sender, RoutedEventArgs e)
+        {
+            BA3.Visibility  = Visibility.Hidden;
+            BXA3.Visibility = Visibility.Visible;
+        }
+
+        private void BB1_Click(object sender, RoutedEventArgs e)
+        {
+            BB1.Visibility  = Visibility.Hidden;
+            BXB1.Visibility = Visibility.Visible;
+        }
+
+        private void BB2_Click(object sender, RoutedEventArgs e)
+        {
+            BB2.Visibility  = Visibility.Hidden;
+            BXB2.Visibility = Visibility.Visible;
+        }
+
+        private void BB3_Click(object sender, RoutedEventArgs e)
+        {
+            BB3.Visibility  = Visibility.Hidden;
+            BXB3.Visibility = Visibility.Visible;
+        }
+
+        private void BC1_Click(object sender, RoutedEventArgs e)
+        {
+            BC1.Visibility  = Visibility.Hidden;
+            BXC1.Visibility = Visibility.Visible;
+        }
+
+        private void BC2_Click(object sender, RoutedEventArgs e)
+        {
+            BC2.Visibility  = Visibility.Hidden;
+            BXC2.Visibility = Visibility.Visible;
+        }
+
+        private void BC3_Click(object sender, RoutedEventArgs e)
+        {
+            BC3.Visibility  = Visibility.Hidden;
+            BXC3.Visibility = Visibility.Visible;
+        }
     }
 }
