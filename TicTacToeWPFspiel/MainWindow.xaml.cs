41eb551a737ab698a832b3bed0838bff55f8181f
Mullins182 <Mullins182@t-online.de>
2023-12-07 23:46:36 +0100

    Unnötige Using-Direktiven entfernt

--- TicTacToeWPFspiel/MainWindow.xaml.cs
+++ TicTacToeWPFspiel/MainWindow.xaml.cs
@@ -1,22 +1,4 @@
-﻿using System.Runtime.ConstrainedExecution;
-using System.Text;
-using System;
-using System.IO;
-using System.Windows;
-using System.Threading;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.ComponentModel;
-using TicTacToeWPFspiel.Properties;
-using System.Reflection;
-using System.Runtime.Intrinsics.X86;
-using System.Security.Policy;
+﻿using System.Windows;
 
 namespace TicTacToeWPFspiel
 {
