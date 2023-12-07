a4919ef6db2a17214f18beb2b2d0fc787d7b2895
Mullins182 <Mullins182@t-online.de>
2023-12-07 12:51:11 +0100

    weitere Funktionen implementiert ...

---
+++ TicTacToeWPFspiel/SpielOptionen.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToeWPFspiel
+{
+    /// <summary>
+    /// Interaktionslogik für SpielOptionen.xaml
+    /// </summary>
+    public partial class SpielOptionen : Page
+    {
+        Frame OptionsFrame;                             // Klassenverfügbare Property, die die Referenz zum Frame speichert !
+
+        public SpielOptionen(Frame OptionsFrame)        // Referenz von OptionsFrame übergeben. (Zeiger)
+        {
+            this.OptionsFrame = OptionsFrame;           // Hier wird die Referenz zu OptionsFrame an Property übergeben 
+            InitializeComponent();
+        }
+
+
+        private void backToMain_Click(object sender, RoutedEventArgs e)
+        {            
+            OptionsFrame.NavigationService.Navigate(new Game());
+        }
+    }
+}
